80ffd076460e1038f1ee2f1882964a4100646c7f
mindsgn <mindsgn@mindsgn.co.za>
2020-02-27 14:38:16 +0200

    camera follow

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float height;
+    public float distance;
+
+    public float rotationDamping;
+    public float heightDamping;
+
+    private void Update()
+    {
+        if (!target) {
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+        } else {
+            var wantedRotationAngle = target.eulerAngles.y;
+            var wantedHeight = transform.position.y + height;
+
+            var currentRotationAngle = transform.eulerAngles.y;
+            var currentHeight = transform.position.y;
+
+            currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+            currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * heightDamping);
+
+            var currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+
+            transform.position = target.position;
+            transform.position -= currentRotation * Vector3.forward * distance;
+
+            transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, rotationDamping * Time.deltaTime);
+        }
+    }
+}
