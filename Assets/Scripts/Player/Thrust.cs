e48b915813caefbb3f95a96d848dfe11d0133244
mindsgn <mindsgn@mindsgn.co.za>
2020-02-27 14:18:48 +0200

    Playership Thrust

---
+++ Assets/Scripts/Player/Thrust.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrust : MonoBehaviour
+{
+    public float Speed = 5.0f;
+
+
+    private void Update()
+    {
+        var Offset = Vector3.forward * Time.deltaTime * Speed;
+        this.transform.Translate(Offset);
+    }
+}
